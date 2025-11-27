68962cac9cfbd98045dc1aa281b517dba9385cad
Walid Alsafadi <117669232+WalidAlsafadi@users.noreply.github.com>
2025-11-27 11:21:39 +0200

    Update allowed CORS origin for frontend app

--- backend/app/main.py
+++ backend/app/main.py
@@ -17,7 +17,7 @@ origins = [
     "http://127.0.0.1:3000",
     "http://localhost:5173",
     "http://127.0.0.1:5173",
-    "https://recipa-rag-assistant.vercel.app",
+    "https://recipaai.vercel.app",
 ]
 
 app.add_middleware(
